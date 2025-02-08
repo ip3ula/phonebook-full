efe6a8846c0be7939542516df9281ca8f53e01f5
ip3ula <ip3ula@hotmail.com>
2025-02-08 12:33:16 +0200

    adds eslint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const Person = require("./models/person")
+const Person = require('./models/person')
 const app = express()
 app.use(express.json())
 morgan.token('type' , (req) => JSON.stringify(req.body))
@@ -13,10 +13,7 @@ app.use(express.static('dist'))
 
 app.get('/info', (request, response) => {
   const time = new Date()
-  Person.find({}).then(data => {
-  response.send(`<p>Phonebook has info for ${data.length} people</p><br/>${time}`)  
-  })
-  
+  Person.find({}).then(data => response.send(`<p>Phonebook has info for ${data.length} people</p><br/>${time}`))
 })
 app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(result => {
@@ -26,26 +23,26 @@ app.get('/api/persons', (request, response, next) => {
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     person
-    ? response.json(person)
-    :response.status(404).end()
+      ? response.json(person)
+      :response.status(404).end()
   }).catch(error => {
     next(error)
   })
 })
 app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-Person.findByIdAndDelete(id).then(result => response.status(204).end()).catch(error => next(error))
+  Person.findByIdAndDelete(id).then(() => response.status(204).end()).catch(error => next(error))
 })
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
   const person = new Person(
-     {
-    name: body.name,
-    number: body.number,
-  }
-    )
-    body.name === undefined
-    ? response.status(400).json({error: 'content missing'})
+    {
+      name: body.name,
+      number: body.number,
+    }
+  )
+  body.name === undefined
+    ? response.status(400).json({ error: 'content missing' })
     : person.save().then(savedPerson => response.json(savedPerson)).catch(error => next(error))
 })
 app.put('/api/persons/:id', (request, response, next) => {
@@ -69,8 +66,6 @@ const errorHandler = (error, request, response, next) => {
 }
 app.use(errorHandler)
 const PORT = process.env.PORT || 3001
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`app is runing at ${PORT}`)
 })
-
-  
--- models/person.js
+++ models/person.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URL
 console.log('connecting to', url)
-mongoose.connect(url).then(result => console.log('connected to mongoose')).catch(error => console.log('error connecting to mongoose:', error.message))
+mongoose.connect(url).then(() => console.log('connected to mongoose')).catch(error => console.log('error connecting to mongoose:', error.message))
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
